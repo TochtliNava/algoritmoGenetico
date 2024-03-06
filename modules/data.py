5de503cd10efa9ad10e45964455ee787f72ce4bd
TochtliNava <tochtlinavadev@gmail.com>
2024-03-05 21:39:53 -0600

    changed font

--- modules/data.py
+++ modules/data.py
@@ -7,7 +7,7 @@ def castDataFrame(root_frame):
     size_frame = Frame(root_frame, bg=colors.WHITE)
     size_frame.pack(side=TOP, fill=BOTH, padx=5, expand=True)
 
-    size_label = Label(size_frame, text="Tamaño del cromosoma", font=("Times new Roman", 15), bg=colors.WHITE)
+    size_label = Label(size_frame, text="Tamaño del cromosoma", font=("Arial", 15), bg=colors.WHITE)
     size_label.pack(side=LEFT, fill=X)
 
     size = Entry(size_frame, justify=RIGHT, bg=colors.WHITE)
@@ -19,7 +19,7 @@ def castDataFrame(root_frame):
     quantity_frame = Frame(root_frame, bg=colors.WHITE)
     quantity_frame.pack(side=TOP, fill=BOTH, padx=5, expand=True)
 
-    quantity_label = Label(quantity_frame, text="Tamaño de la poblacion", font=("Times new Roman", 15), bg=colors.WHITE)
+    quantity_label = Label(quantity_frame, text="Tamaño de la poblacion", font=("Arial", 15), bg=colors.WHITE)
     quantity_label.pack(side=LEFT, fill=X)
 
     quantity = Entry(quantity_frame, justify=RIGHT, bg=colors.WHITE)
@@ -30,7 +30,7 @@ def castDataFrame(root_frame):
     gen_frame = Frame(root_frame, bg=colors.WHITE)
     gen_frame.pack(side=TOP, fill=BOTH, padx=5, expand=True)
 
-    gen_label = Label(gen_frame, text="Numero de Generaciones", font=("Times new Roman", 15), bg=colors.WHITE)
+    gen_label = Label(gen_frame, text="Numero de Generaciones", font=("Arial", 15), bg=colors.WHITE)
     gen_label.pack(side=LEFT, fill=X)
 
     gen = Entry(gen_frame, justify=RIGHT, bg=colors.WHITE)
